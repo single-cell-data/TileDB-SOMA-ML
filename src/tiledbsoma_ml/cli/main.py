d99ebe44a5cfaa10f2afa0685decbe3e9bb47477
Ryan Williams <ryan.williams@tiledb.com>
2024-12-04 03:28:37 +0000

    add cpu mode

--- src/tiledbsoma_ml/cli/main.py
+++ src/tiledbsoma_ml/cli/main.py
@@ -19,16 +19,21 @@ def tdbsml():
 
 
 @tdbsml.command
+@option('-c', '--cpu', is_flag=True, help="Force CPU mode")
 @option('-l', '--use-lightning', is_flag=True, help="Use PyTorch Lightning for training")
 @option('-t', '--tissue', help='"tissue_general" filter to pass to obs_value_filter')
 @option('-w', '--workers', type=int, help="Number of GPU workers to use")
 @argument('in_nb_path')
 @argument('out_nb_path', required=False)
-def benchmark(use_lightning, tissue, workers, in_nb_path, out_nb_path):
+def benchmark(cpu, use_lightning, tissue, workers, in_nb_path, out_nb_path):
     if not out_nb_path:
         name = "lightning" if use_lightning else "torch"
         if workers is not None:
             name += f"{workers}"
+            if cpu:
+                raise ValueError("Specify -c/--cpu xor -w/--workers")
+        elif cpu:
+            name += "-cpu"
         makedirs(tissue, exist_ok=True)
         out_nb_path = f"{tissue}/{name}.ipynb"
         err(f"Using {out_nb_path=}")
@@ -39,6 +44,7 @@ def benchmark(use_lightning, tissue, workers, in_nb_path, out_nb_path):
             "/usr/bin/time", "-v", "-o", tmpfile,
             "papermill",
             "-p", "tissue", tissue,
+            *(["-p", "cpu", f"{cpu}"] if cpu else []),
             *(["-p", "lightning", f"{use_lightning}"] if use_lightning else []),
             *([] if workers is None else ["-p", "workers", f"{workers}"]),
             in_nb_path,

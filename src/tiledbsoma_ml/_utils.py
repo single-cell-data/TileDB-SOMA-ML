2af96edfea211942b09bd519c42c1d151b4e5a4c
Ryan Williams <ryan.williams@tiledb.com>
2024-12-15 11:18:12 -0500

    `_csr.py`

--- src/tiledbsoma_ml/_utils.py
+++ src/tiledbsoma_ml/_utils.py
@@ -6,12 +6,13 @@
 import itertools
 import sys
 from itertools import islice
-from typing import Iterable, Iterator, Tuple, TypeVar
+from typing import Any, Iterable, Iterator, Tuple, TypeVar
 
 import numpy as np
 import numpy.typing as npt
 
 _T_co = TypeVar("_T_co", covariant=True)
+NDArrayNumber = npt.NDArray[np.number[Any]]
 
 
 def splits(total_length: int, sections: int) -> npt.NDArray[np.intp]:
--- src/tiledbsoma_ml/pytorch.py
+++ src/tiledbsoma_ml/pytorch.py
@@ -8,7 +8,6 @@ from __future__ import annotations
 import contextlib
 import gc
 import logging
-import math
 import os
 import time
 from math import ceil
@@ -17,15 +16,12 @@ from typing import (
     ContextManager,
     Iterable,
     Iterator,
-    List,
     Sequence,
     Tuple,
-    Type,
     TypeVar,
     Union,
 )
 
-import numba
 import numpy as np
 import numpy.typing as npt
 import pandas as pd
@@ -35,23 +31,21 @@ import tiledbsoma as soma
 import torch
 import torchdata
 from somacore.query._eager_iter import EagerIterator as _EagerIterator
-from typing_extensions import Self
 
+from tiledbsoma_ml._csr import CSR_IO_Buffer
 from tiledbsoma_ml._distributed import (
     get_distributed_world_rank,
     get_worker_world_rank,
     init_multiprocessing,
 )
 from tiledbsoma_ml._experiment_locator import ExperimentLocator
-from tiledbsoma_ml._utils import batched, splits
+from tiledbsoma_ml._utils import NDArrayNumber, batched, splits
 
 logger = logging.getLogger("tiledbsoma_ml.pytorch")
 
 _T = TypeVar("_T")
 
-NDArrayNumber = npt.NDArray[np.number[Any]]
 NDArrayJoinId = npt.NDArray[np.int64]
-_CSRIdxArray = npt.NDArray[np.unsignedinteger[Any]]
 XBatch = Union[NDArrayNumber, sparse.csr_matrix]
 Batch = Tuple[XBatch, pd.DataFrame]
 """"Batch" type yielded by ``ExperimentAxisQueryIterableDataset`` and ``ExperimentAxisQueryIterDataPipe``;
@@ -403,7 +397,7 @@ class ExperimentAxisQueryIterable(Iterable[Batch]):
         obs: soma.DataFrame,
         X: soma.SparseNDArray,
         obs_joinid_iter: Iterator[NDArrayJoinId],
-    ) -> Iterator[Tuple[_CSR_IO_Buffer, pd.DataFrame]]:
+    ) -> Iterator[Tuple[CSR_IO_Buffer, pd.DataFrame]]:
         """Iterate over IO batches, i.e., SOMA query reads, producing tuples of ``(X: csr_array, obs: DataFrame)``.
 
         ``obs`` joinids read are controlled by the ``obs_joinid_iter``. Iterator results will be reindexed and shuffled
@@ -448,9 +442,9 @@ class ExperimentAxisQueryIterable(Iterable[Batch]):
                 obs_coords: NDArrayJoinId,
                 var_coords: NDArrayJoinId,
                 obs_indexer: soma.IntIndexer,
-            ) -> _CSR_IO_Buffer:
+            ) -> CSR_IO_Buffer:
                 """This function provides a GC after we throw off (large) garbage."""
-                m = _CSR_IO_Buffer.from_ijd(
+                m = CSR_IO_Buffer.from_ijd(
                     obs_indexer.get_indexer(X_tbl["soma_dim_0"]),
                     var_indexer.get_indexer(X_tbl["soma_dim_1"]),
                     X_tbl["soma_data"].to_numpy(),
@@ -478,7 +472,7 @@ class ExperimentAxisQueryIterable(Iterable[Batch]):
                 [self.obs_column_names]
             )  # fmt: on
 
-            X_io_batch = _CSR_IO_Buffer.merge(tuple(_io_buf_iter))
+            X_io_batch = CSR_IO_Buffer.merge(tuple(_io_buf_iter))
 
             del obs_indexer, obs_coords, obs_shuffled_coords, _io_buf_iter
             gc.collect()
@@ -945,258 +939,3 @@ def _collate_noop(datum: _T) -> _T:
     Private.
     """
     return datum
-
-
-class _CSR_IO_Buffer:
-    """Implement a minimal CSR matrix with specific optimizations for use in this package.
-
-    Operations supported are:
-    * Incrementally build a CSR from COO, allowing overlapped I/O and CSR conversion for I/O batches,
-      and a final "merge" step which combines the result.
-    * Zero intermediate copy conversion of an arbitrary row slice to dense (i.e., mini-batch extraction).
-    * Parallel processing, where possible (construction, merge, etc.).
-    * Minimize memory use for index arrays.
-
-    Overall is significantly faster, and uses less memory, than the equivalent ``scipy.sparse`` operations.
-    """
-
-    __slots__ = ("indptr", "indices", "data", "shape")
-
-    def __init__(
-        self,
-        indptr: _CSRIdxArray,
-        indices: _CSRIdxArray,
-        data: NDArrayNumber,
-        shape: Tuple[int, int],
-    ) -> None:
-        """Construct from PJV format."""
-        assert len(data) == len(indices)
-        assert len(data) <= np.iinfo(indptr.dtype).max
-        assert shape[1] <= np.iinfo(indices.dtype).max
-        assert indptr[-1] == len(data) and indptr[0] == 0
-
-        self.shape = shape
-        self.indptr = indptr
-        self.indices = indices
-        self.data = data
-
-    @staticmethod
-    def from_ijd(
-        i: _CSRIdxArray, j: _CSRIdxArray, d: NDArrayNumber, shape: Tuple[int, int]
-    ) -> _CSR_IO_Buffer:
-        """Factory from COO"""
-        nnz = len(d)
-        indptr: _CSRIdxArray = np.zeros((shape[0] + 1), dtype=smallest_uint_dtype(nnz))
-        indices: _CSRIdxArray = np.empty((nnz,), dtype=smallest_uint_dtype(shape[1]))
-        data = np.empty((nnz,), dtype=d.dtype)
-        _coo_to_csr_inner(shape[0], i, j, d, indptr, indices, data)
-        return _CSR_IO_Buffer(indptr, indices, data, shape)
-
-    @staticmethod
-    def from_pjd(
-        p: _CSRIdxArray, j: _CSRIdxArray, d: NDArrayNumber, shape: Tuple[int, int]
-    ) -> _CSR_IO_Buffer:
-        """Factory from CSR"""
-        return _CSR_IO_Buffer(p, j, d, shape)
-
-    @property
-    def nnz(self) -> int:
-        return len(self.indices)
-
-    @property
-    def nbytes(self) -> int:
-        return int(self.indptr.nbytes + self.indices.nbytes + self.data.nbytes)
-
-    @property
-    def dtype(self) -> npt.DTypeLike:
-        return self.data.dtype
-
-    def slice_tonumpy(self, row_index: slice) -> NDArrayNumber:
-        """Extract slice as a dense ndarray. Does not assume any particular ordering of minor axis."""
-        assert isinstance(row_index, slice)
-        assert row_index.step in (1, None)
-        row_idx_start, row_idx_end, _ = row_index.indices(self.indptr.shape[0] - 1)
-        n_rows = max(row_idx_end - row_idx_start, 0)
-        out = np.zeros((n_rows, self.shape[1]), dtype=self.data.dtype)
-        if n_rows >= 0:
-            _csr_to_dense_inner(
-                row_idx_start, n_rows, self.indptr, self.indices, self.data, out
-            )
-        return out
-
-    def slice_toscipy(self, row_index: slice) -> sparse.csr_matrix:
-        """Extract slice as a ``sparse.csr_matrix``. Does not assume any particular ordering of
-        minor axis, but will return a canonically ordered scipy sparse object."""
-        assert isinstance(row_index, slice)
-        assert row_index.step in (1, None)
-        row_idx_start, row_idx_end, _ = row_index.indices(self.indptr.shape[0] - 1)
-        n_rows = max(row_idx_end - row_idx_start, 0)
-        if n_rows == 0:
-            return sparse.csr_matrix((0, self.shape[1]), dtype=self.dtype)
-
-        indptr = self.indptr[row_idx_start : row_idx_end + 1].copy()
-        indices = self.indices[indptr[0] : indptr[-1]].copy()
-        data = self.data[indptr[0] : indptr[-1]].copy()
-        indptr -= indptr[0]
-        return sparse.csr_matrix((data, indices, indptr), shape=(n_rows, self.shape[1]))
-
-    @staticmethod
-    def merge(mtxs: Sequence[_CSR_IO_Buffer]) -> _CSR_IO_Buffer:
-        assert len(mtxs) > 0
-        nnz = sum(m.nnz for m in mtxs)
-        shape = mtxs[0].shape
-        for m in mtxs[1:]:
-            assert m.shape == mtxs[0].shape
-            assert m.indices.dtype == mtxs[0].indices.dtype
-        assert all(m.shape == shape for m in mtxs)
-
-        indptr = np.sum(
-            [m.indptr for m in mtxs], axis=0, dtype=smallest_uint_dtype(nnz)
-        )
-        indices = np.empty((nnz,), dtype=mtxs[0].indices.dtype)
-        data = np.empty((nnz,), mtxs[0].data.dtype)
-
-        _csr_merge_inner(
-            tuple((m.indptr.astype(indptr.dtype), m.indices, m.data) for m in mtxs),
-            indptr,
-            indices,
-            data,
-        )
-        return _CSR_IO_Buffer.from_pjd(indptr, indices, data, shape)
-
-    def sort_indices(self) -> Self:
-        """Sort indices, IN PLACE."""
-        _csr_sort_indices(self.indptr, self.indices, self.data)
-        return self
-
-
-def smallest_uint_dtype(max_val: int) -> Type[np.unsignedinteger[Any]]:
-    dts: List[Type[np.unsignedinteger[Any]]] = [np.uint16, np.uint32]
-    for dt in dts:
-        if max_val <= np.iinfo(dt).max:
-            return dt
-    else:
-        return np.uint64
-
-
-@numba.njit(nogil=True, parallel=True)  # type:ignore[misc]
-def _csr_merge_inner(
-    As: Tuple[Tuple[_CSRIdxArray, _CSRIdxArray, NDArrayNumber], ...],  # P,J,D
-    Bp: _CSRIdxArray,
-    Bj: _CSRIdxArray,
-    Bd: NDArrayNumber,
-) -> None:
-    n_rows = len(Bp) - 1
-    offsets = Bp.copy()
-    for Ap, Aj, Ad in As:
-        n_elmts = Ap[1:] - Ap[:-1]
-        for n in numba.prange(n_rows):
-            Bj[offsets[n] : offsets[n] + n_elmts[n]] = Aj[Ap[n] : Ap[n] + n_elmts[n]]
-            Bd[offsets[n] : offsets[n] + n_elmts[n]] = Ad[Ap[n] : Ap[n] + n_elmts[n]]
-        offsets[:-1] += n_elmts
-
-
-@numba.njit(nogil=True, parallel=True)  # type:ignore[misc]
-def _csr_to_dense_inner(
-    row_idx_start: int,
-    n_rows: int,
-    indptr: _CSRIdxArray,
-    indices: _CSRIdxArray,
-    data: NDArrayNumber,
-    out: NDArrayNumber,
-) -> None:
-    for i in numba.prange(row_idx_start, row_idx_start + n_rows):
-        for j in range(indptr[i], indptr[i + 1]):
-            out[i - row_idx_start, indices[j]] = data[j]
-
-
-@numba.njit(nogil=True, parallel=True, inline="always")  # type:ignore[misc]
-def _count_rows(n_rows: int, Ai: NDArrayNumber, Bp: NDArrayNumber) -> NDArrayNumber:
-    """Private: parallel row count."""
-    nnz = len(Ai)
-
-    partition_size = 32 * 1024**2
-    n_partitions = math.ceil(nnz / partition_size)
-    if n_partitions > 1:
-        counts = np.zeros((n_partitions, n_rows), dtype=Bp.dtype)
-        for p in numba.prange(n_partitions):
-            for n in range(p * partition_size, min(nnz, (p + 1) * partition_size)):
-                row = Ai[n]
-                counts[p, row] += 1
-
-        Bp[:-1] = counts.sum(axis=0)
-    else:
-        for n in range(nnz):
-            row = Ai[n]
-            Bp[row] += 1
-
-    return Bp
-
-
-@numba.njit(nogil=True, parallel=True)  # type:ignore[misc]
-def _coo_to_csr_inner(
-    n_rows: int,
-    Ai: _CSRIdxArray,
-    Aj: _CSRIdxArray,
-    Ad: NDArrayNumber,
-    Bp: _CSRIdxArray,
-    Bj: _CSRIdxArray,
-    Bd: NDArrayNumber,
-) -> None:
-    nnz = len(Ai)
-
-    _count_rows(n_rows, Ai, Bp)
-
-    # cum sum to get the row index pointers (NOTE: starting with zero)
-    cumsum = 0
-    for n in range(n_rows):
-        tmp = Bp[n]
-        Bp[n] = cumsum
-        cumsum += tmp
-    Bp[n_rows] = nnz
-
-    # Reorganize all the data. Side effect: pointers shifted (reversed in the
-    # subsequent section).
-    #
-    # Method is concurrent (partitioned by rows) if number of rows is greater
-    # than 2**partition_bits. This partitioning scheme leverages the fact
-    # that reads are much cheaper than writes.
-    #
-    # The code is equivalent to:
-    #   for n in range(nnz):
-    #       row = Ai[n]
-    #       dst_row = Bp[row]
-    #       Bj[dst_row] = Aj[n]
-    #       Bd[dst_row] = Ad[n]
-    #       Bp[row] += 1
-
-    partition_bits = 13
-    n_partitions = (n_rows + 2**partition_bits - 1) >> partition_bits
-    for p in numba.prange(n_partitions):
-        for n in range(nnz):
-            row = Ai[n]
-            if (row >> partition_bits) != p:
-                continue
-            dst_row = Bp[row]
-            Bj[dst_row] = Aj[n]
-            Bd[dst_row] = Ad[n]
-            Bp[row] += 1
-
-    # Shift the pointers by one slot (i.e., start at zero)
-    prev_ptr = 0
-    for n in range(n_rows + 1):
-        tmp = Bp[n]
-        Bp[n] = prev_ptr
-        prev_ptr = tmp
-
-
-@numba.njit(nogil=True, parallel=True)  # type:ignore[misc]
-def _csr_sort_indices(Bp: _CSRIdxArray, Bj: _CSRIdxArray, Bd: NDArrayNumber) -> None:
-    """In-place sort of minor axis indices"""
-    n_rows = len(Bp) - 1
-    for r in numba.prange(n_rows):
-        row_start = Bp[r]
-        row_end = Bp[r + 1]
-        order = np.argsort(Bj[row_start:row_end])
-        Bj[row_start:row_end] = Bj[row_start:row_end][order]
-        Bd[row_start:row_end] = Bd[row_start:row_end][order]

ee15dbceb5de7c4ca188606d6191f3f056fe86d8
Ryan Williams <ryan.williams@tiledb.com>
2025-01-09 11:50:18 -0500

    test_dataloader nits

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -4,16 +4,16 @@
 # Licensed under the MIT License.
 
 from functools import partial
-from typing import Any, Tuple
+from typing import Tuple
 from unittest.mock import patch
 
 import numpy as np
-import numpy.typing as npt
 import pandas as pd
 import pytest
 from tiledbsoma import Experiment
 
 from tests._utils import assert_array_equal, parametrize, pytorch_x_value_gen
+from tiledbsoma_ml.common import NDArrayNumber
 from tiledbsoma_ml.dataloader import experiment_dataloader
 from tiledbsoma_ml.dataset import ExperimentDataset
 
@@ -41,10 +41,7 @@ def test_multiprocessing__returns_full_result(soma_experiment: Experiment):
         assert sorted(soma_joinids) == list(range(6))
 
 
-@parametrize(
-    "obs_range,var_range,X_value_gen",
-    [(3, 3, pytorch_x_value_gen)],
-)
+@parametrize("obs_range,var_range,X_value_gen", [(3, 3, pytorch_x_value_gen)])
 @parametrize("use_eager_fetch", [True, False])
 def test_experiment_dataloader__non_batched(
     soma_experiment: Experiment,
@@ -90,10 +87,7 @@ def test_experiment_dataloader__batched(
         assert obs.to_numpy().tolist() == [[0], [1], [2]]
 
 
-@parametrize(
-    "obs_range,var_range,X_value_gen",
-    [(10, 3, pytorch_x_value_gen)],
-)
+@parametrize("obs_range,var_range,X_value_gen", [(10, 3, pytorch_x_value_gen)])
 @parametrize("use_eager_fetch", [True, False])
 def test_experiment_dataloader__batched_length(
     soma_experiment: Experiment,
@@ -112,17 +106,15 @@ def test_experiment_dataloader__batched_length(
         assert len(dl) == len(list(dl))
 
 
-@parametrize(
-    "obs_range,var_range,X_value_gen,batch_size",
-    [(10, 3, pytorch_x_value_gen, batch_size) for batch_size in (1, 3, 10)],
-)
+@parametrize("obs_range,var_range,X_value_gen", [(10, 3, pytorch_x_value_gen)])
+@parametrize("batch_size", [1, 3, 10])
 def test_experiment_dataloader__collate_fn(
     soma_experiment: Experiment,
     batch_size: int,
 ):
     def collate_fn(
-        batch_size: int, data: Tuple[npt.NDArray[np.number[Any]], pd.DataFrame]
-    ) -> Tuple[npt.NDArray[np.number[Any]], pd.DataFrame]:
+        batch_size: int, data: Tuple[NDArrayNumber, pd.DataFrame]
+    ) -> Tuple[NDArrayNumber, pd.DataFrame]:
         assert isinstance(data, tuple)
         assert len(data) == 2
         assert isinstance(data[0], np.ndarray) and isinstance(data[1], pd.DataFrame)

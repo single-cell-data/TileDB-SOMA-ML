ba43d4f6a1ea10a0610b2d512633fc520c1bdd7f
Ryan Williams <ryan.williams@tiledb.com>
2025-01-15 22:35:31 -0500

    `test_csr`: import/assert tweaks

--- tests/test_csr.py
+++ tests/test_csr.py
@@ -8,9 +8,12 @@ from typing import Tuple
 
 import numpy as np
 import numpy.typing as npt
+from numpy.testing import assert_array_equal
 from scipy import sparse
+from scipy.sparse import csr_matrix
 
 from tests._utils import parametrize
+from tiledbsoma_ml._csr import CSR_IO_Buffer
 
 
 @parametrize(  # keep these small as we materialize as a dense ndarray
@@ -19,8 +22,6 @@ from tests._utils import parametrize
 )
 @parametrize("dtype", [np.float32, np.float64, np.int32])
 def test_construct_from_ijd(shape: Tuple[int, int], dtype: npt.DTypeLike):
-    from tiledbsoma_ml._csr import CSR_IO_Buffer
-
     sp_coo = sparse.random(shape[0], shape[1], dtype=dtype, format="coo", density=0.05)
     sp_csr = sp_coo.tocsr()
 
@@ -36,16 +37,16 @@ def test_construct_from_ijd(shape: Tuple[int, int], dtype: npt.DTypeLike):
     # CSR_IO_Buffer makes no guarantees about minor axis ordering (ie, "canonical" form) until
     # sort_indices is called, so use the SciPy sparse csr package to validate by round-tripping.
     assert (
-        sparse.csr_matrix((_ncsr.data, _ncsr.indices, _ncsr.indptr), shape=_ncsr.shape)
+        csr_matrix((_ncsr.data, _ncsr.indices, _ncsr.indptr), shape=_ncsr.shape)
         != sp_csr
     ).nnz == 0
 
     # Check dense slicing
-    assert np.array_equal(_ncsr.slice_tonumpy(slice(0, shape[0])), sp_coo.toarray())
-    assert np.array_equal(_ncsr.slice_tonumpy(slice(0, shape[0])), sp_csr.toarray())
-    assert np.array_equal(_ncsr.slice_tonumpy(slice(1, -1)), sp_csr[1:-1].toarray())
-    assert np.array_equal(_ncsr.slice_tonumpy(slice(None, -2)), sp_csr[:-2].toarray())
-    assert np.array_equal(_ncsr.slice_tonumpy(slice(None)), sp_csr[:].toarray())
+    assert_array_equal(_ncsr.slice_tonumpy(slice(0, shape[0])), sp_coo.toarray())
+    assert_array_equal(_ncsr.slice_tonumpy(slice(0, shape[0])), sp_csr.toarray())
+    assert_array_equal(_ncsr.slice_tonumpy(slice(1, -1)), sp_csr[1:-1].toarray())
+    assert_array_equal(_ncsr.slice_tonumpy(slice(None, -2)), sp_csr[:-2].toarray())
+    assert_array_equal(_ncsr.slice_tonumpy(slice(None)), sp_csr[:].toarray())
 
     # Check sparse slicing
     assert (_ncsr.slice_toscipy(slice(0, shape[0])) != sp_csr).nnz == 0
@@ -60,8 +61,6 @@ def test_construct_from_ijd(shape: Tuple[int, int], dtype: npt.DTypeLike):
 )
 @parametrize("dtype", [np.float32, np.float64, np.int32])
 def test_construct_from_pjd(shape: Tuple[int, int], dtype: npt.DTypeLike):
-    from tiledbsoma_ml._csr import CSR_IO_Buffer
-
     sp_csr = sparse.random(shape[0], shape[1], dtype=dtype, format="csr", density=0.05)
 
     _ncsr = CSR_IO_Buffer.from_pjd(
@@ -74,15 +73,15 @@ def test_construct_from_pjd(shape: Tuple[int, int], dtype: npt.DTypeLike):
     # CSR_IO_Buffer makes no guarantees about minor axis ordering (ie, "canonical" form) until
     # sort_indices is called, so use the SciPy sparse csr package to validate by round-tripping.
     assert (
-        sparse.csr_matrix((_ncsr.data, _ncsr.indices, _ncsr.indptr), shape=_ncsr.shape)
+        csr_matrix((_ncsr.data, _ncsr.indices, _ncsr.indptr), shape=_ncsr.shape)
         != sp_csr
     ).nnz == 0
 
     # Check dense slicing
-    assert np.array_equal(_ncsr.slice_tonumpy(slice(0, shape[0])), sp_csr.toarray())
-    assert np.array_equal(_ncsr.slice_tonumpy(slice(1, -1)), sp_csr[1:-1].toarray())
-    assert np.array_equal(_ncsr.slice_tonumpy(slice(None, -2)), sp_csr[:-2].toarray())
-    assert np.array_equal(_ncsr.slice_tonumpy(slice(None)), sp_csr[:].toarray())
+    assert_array_equal(_ncsr.slice_tonumpy(slice(0, shape[0])), sp_csr.toarray())
+    assert_array_equal(_ncsr.slice_tonumpy(slice(1, -1)), sp_csr[1:-1].toarray())
+    assert_array_equal(_ncsr.slice_tonumpy(slice(None, -2)), sp_csr[:-2].toarray())
+    assert_array_equal(_ncsr.slice_tonumpy(slice(None)), sp_csr[:].toarray())
 
     # Check sparse slicing
     assert (_ncsr.slice_toscipy(slice(0, shape[0])) != sp_csr).nnz == 0
@@ -98,8 +97,6 @@ def test_construct_from_pjd(shape: Tuple[int, int], dtype: npt.DTypeLike):
 @parametrize("dtype", [np.float32, np.float64, np.int32])
 @parametrize("n_splits", [2, 3, 4])
 def test_merge(shape: Tuple[int, int], dtype: npt.DTypeLike, n_splits: int):
-    from tiledbsoma_ml._csr import CSR_IO_Buffer
-
     sp_coo = sparse.random(shape[0], shape[1], dtype=dtype, format="coo", density=0.5)
     splits = [
         t
@@ -116,9 +113,7 @@ def test_merge(shape: Tuple[int, int], dtype: npt.DTypeLike, n_splits: int):
 
     assert (
         sp_coo.tocsr()
-        != sparse.csr_matrix(
-            (_ncsr.data, _ncsr.indices, _ncsr.indptr), shape=_ncsr.shape
-        )
+        != csr_matrix((_ncsr.data, _ncsr.indices, _ncsr.indptr), shape=_ncsr.shape)
     ).nnz == 0
 
 
@@ -127,8 +122,6 @@ def test_merge(shape: Tuple[int, int], dtype: npt.DTypeLike, n_splits: int):
     [(100, 10), (10, 100), (1, 1), (1, 100), (100, 1), (0, 0), (10, 0), (0, 10)],
 )
 def test_sort_indices(shape: Tuple[int, int]):
-    from tiledbsoma_ml._csr import CSR_IO_Buffer
-
     sp_coo = sparse.random(
         shape[0], shape[1], dtype=np.float32, format="coo", density=0.05
     )
@@ -138,6 +131,6 @@ def test_sort_indices(shape: Tuple[int, int]):
         sp_coo.row, sp_coo.col, sp_coo.data, shape=sp_coo.shape
     ).sort_indices()
 
-    assert np.array_equal(sp_csr.indptr, _ncsr.indptr)
-    assert np.array_equal(sp_csr.indices, _ncsr.indices)
-    assert np.array_equal(sp_csr.data, _ncsr.data)
+    assert_array_equal(sp_csr.indptr, _ncsr.indptr)
+    assert_array_equal(sp_csr.indices, _ncsr.indices)
+    assert_array_equal(sp_csr.data, _ncsr.data)

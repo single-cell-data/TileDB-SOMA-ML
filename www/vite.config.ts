39cf6fcc6903b32aee872a62512b354690a509df
Ryan Williams <ryan.williams@tiledb.com>
2025-01-16 19:41:00 -0500

    serve from `/TileDB-SOMA-ML`

--- www/vite.config.ts
+++ www/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isCI = process.env['CI']
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  ...(isCI ? { base: '/TileDB-SOMA-ML/', } : {}),
+  build: {
+    outDir: isCI ? 'dist' : 'dist/TileDB-SOMA-ML',
+  }
 })

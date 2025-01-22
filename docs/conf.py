295fceefdec6b41a6c2b25f7c1955cf376c0ef3e
Ryan Williams <ryan.williams@tiledb.com>
2025-01-22 10:55:13 -0500

    `IOBatches`

--- docs/conf.py
+++ docs/conf.py
@@ -69,6 +69,9 @@ rst_prolog = """
 .. |QueryIDs| replace:: :class:`~tiledbsoma_ml.query_ids.QueryIDs`
 .. |Q.obs_joinids| replace:: :obj:`~tiledbsoma_ml.query_ids.QueryIDs.obs_joinids`
 .. |QueryIDs.shuffle_chunks| replace:: :meth:`QueryIDs.shuffle_chunks <tiledbsoma_ml.query_ids.QueryIDs.shuffle_chunks>`
+.. |Chunks| replace:: :class:`~tiledbsoma_ml.query_ids.Chunks`
+.. |IOBatch| replace:: :class:`~tiledbsoma_ml.io_batches.IOBatch`
+.. |IOBatches| replace:: :class:`~tiledbsoma_ml.io_batches.IOBatches`
 .. |CSR_IO_Buffer| replace:: :class:`~tiledbsoma_ml._csr.CSR_IO_Buffer`
 .. |get_distributed_world_rank| replace:: :obj:`~tiledbsoma_ml._distributed.get_distributed_world_rank`
 .. |get_worker_world_rank| replace:: :obj:`~tiledbsoma_ml._distributed.get_worker_world_rank`

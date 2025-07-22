7bdb4aec95ab89f498f0d50c26ed42fcc3cedef7
EC2 Default User <ec2-user@ip-172-31-36-224.ec2.internal>
2025-07-22 17:01:42 +0000

    New Defaults

--- src/tiledbsoma_ml/scvi.py
+++ src/tiledbsoma_ml/scvi.py
@@ -12,6 +12,11 @@ from torch.utils.data import DataLoader
 from tiledbsoma_ml import ExperimentDataset, experiment_dataloader
 from tiledbsoma_ml._common import MiniBatch
 
+DEFAULT_DATALOADER_KWARGS: dict[str, Any] = {
+    "pin_memory": torch.cuda.is_available(),
+    "persistent_workers": True,
+    "num_workers": max(os.cpu_count() // 2, 1),
+}
 
 class SCVIDataModule(LightningDataModule):  # type: ignore[misc]
     """PyTorch Lightning DataModule for training scVI models from SOMA data.
@@ -61,9 +66,10 @@ class SCVIDataModule(LightningDataModule):  # type: ignore[misc]
         self.query = query
         self.dataset_args = args
         self.dataset_kwargs = kwargs
-        self.dataloader_kwargs = (
-            dataloader_kwargs if dataloader_kwargs is not None else {}
-        )
+        self.dataloader_kwargs = {
+            **DEFAULT_DATALOADER_KWARGS,
+            **(dataloader_kwargs or {}),
+        }
         self.batch_column_names = (
             batch_column_names
             if batch_column_names is not None

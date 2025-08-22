4c4327961fbfd3758c807d328903a5f01bbc30f9
Aadit Saluja <aaditsaluja@gmail.com>
2025-08-22 15:14:44 +0000

    Added Range

--- tests/test_gpu_shuffle.py
+++ tests/test_gpu_shuffle.py
@@ -1,8 +1,35 @@
 from pytest import fixture
+import torch
 
 @fixture
 def check(check_gpu):
     return check_gpu
 
+@param(obs_range=40, shuffle_chunk_size=2, io_batch_size=4, batch_size=3,
+       shuffle=True, shuffle_mode="gpu_iobatch", device=torch.device("cuda", 0))
+@parametrize("seed,rank,world_size,num_workers", [
+    (False, 0, 2, 2),
+    (False, 1, 2, 2),
+    (  111, 0, 2, 2),
+    (  111, 1, 2, 2),
+])
 def test_gpu_iobatch_worker_partitioning_even(check):
+    pass
+
+@param(obs_range=41, shuffle_chunk_size=2, io_batch_size=4, batch_size=3,
+       shuffle=True, shuffle_mode="gpu_iobatch", device=torch.device("cuda", 0))
+@parametrize("seed,rank,world_size,num_workers", [
+    (False, 0, 2, 2),
+    (False, 1, 2, 2),
+])
+def test_gpu_iobatch_worker_partitioning_drop1(check):
+    pass
+
+@param(obs_range=42, shuffle_chunk_size=2, io_batch_size=4, batch_size=3,
+       shuffle=True, shuffle_mode="gpu_iobatch", device=torch.device("cuda", 0))
+@parametrize("seed,rank,world_size,num_workers", [
+    (False, 0, 2, 2),
+    (False, 1, 2, 2),
+])
+def test_gpu_iobatch_worker_partitioning_uneven_workers(check):
     pass
